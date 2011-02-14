b2e4ac1bac50cb1cd1df9c23ac2f71016b6c812e
Ondross <andrew.heine@students.olin.edu>
2011-02-14 13:52:24 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parkforme::Application.config.secret_token = '06309fa4f78416c548a0be744adb3a8464a8d96eab6565af6b758097e7225a4a345788875b1b77a4d22999417eeb64cbda9cdc5279f422bcb736e204bb028516'
